cc7bda51108a39ceb45287d250853076dde3f5ed
root <emmanuelmajor99@gmail.com>
2022-10-13 17:54:57 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
+ *  main - main block
  *
- *  *   main - main block
- *
- *   *
- *
- *    *   Return: 0
- *
- *     */
-
+ *  Return: 0
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
