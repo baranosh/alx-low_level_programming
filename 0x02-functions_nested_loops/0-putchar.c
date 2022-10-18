ce2380feb060ba6b800d3f2f2a0b08ea5927cca9
baranosh <emmanuelmajor99@gmail.com>
2022-10-18 21:11:34 +0000

    Write a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry
+ * Writing a script that prints _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar("_putchar");
+
+	putchar("\n");
+	return (0);
+}
