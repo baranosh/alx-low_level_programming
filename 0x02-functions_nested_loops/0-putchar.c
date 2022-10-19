6dab1c63356bb1b727607ceaf571cda162b37a38
baranosh <emmanuelmajor99@gmail.com>
2022-10-19 05:07:58 +0000

    Writes a script that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
-/**
- * main - check the code
- * Description: Writes a code that prints _putchar followed by a new line
+/** main - Task 0
+ * Write a code that prints _putchar
  * Return: Always 0
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	putchar("\n");
+	
+	return (0);
 }
