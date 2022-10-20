261868a4a816c58f3347a8492806234d41b0fa0a
Bar_anosh <emmanuelmajor99@gmail.com>
2022-10-20 05:54:07 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _slower(int c);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
