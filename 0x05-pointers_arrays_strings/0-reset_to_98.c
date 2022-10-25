c150ff8f84e1e86c5a10545e792ccb95f5383872
Bar_anosh <emmanuelmajor99@gmail.com>
2022-10-25 05:47:30 +0100

    98 Battery st.
    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - write a function that takes a pointer to an int parameter and
+ * reset_to_98 - write a function that takes a pointer to an int parameter and
  * updates the value to it points 98
  * @n: pointer.
  * Return: void
