664ea32503fc694d7079295c6c09896a21fa6535
Bar_anosh <emmanuelmajor99@gmail.com>
2022-10-25 06:53:59 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
    Write a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * Main - I sometimes suffer from Insomnia
  * and all I can do is just read 
  * 
  * Return: Always 0
  */
+
 void print_alphabet(void)
 {
 char atoz = 'a';
