21304b6c4c6a3320c09a71856b19bac324e5fefc
Bar_anosh <emmanuelmajor99@gmail.com>
2022-10-25 05:53:24 +0100

    This report, by its very length, defends itself against the risk of being read
    Write a function that returns the length of a string.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
 /**
 * _strlen - find the lenght of a string
 * @s: pointer to the string to chack
 * Return: void
 */
-
 int _strlen(char *s)
 {
 	int i = 0;
-
 	while (s[i])
 	i++;
-
 	return (i);
 }
