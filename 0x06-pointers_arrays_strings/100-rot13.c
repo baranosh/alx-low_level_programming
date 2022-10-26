905970c41a6a322df1d84f51696c6523966d2cc5
Bar_anosh <emmanuelmajor99@gmail.com>
2022-10-26 23:16:47 +0100

    rot-13
    Write a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,18 @@
+] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
