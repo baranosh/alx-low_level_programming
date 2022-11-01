8195b8e707c768a436005e0eb39ea6b05717596c
Bar_anosh <emmanuelmajor99@gmail.com>
2022-11-01 08:32:51 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - prints sum of #'s in diagnols of square
- * @a: input square array
- * @size: size of one dimension in array
- * Return: void
+ * print_diagsums -  prints sums
+ * @a: a pointer
+ * @size: size
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int j;
-	sum1 = 0;
-	sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
-		sum1 += a[j], sum2 += a[i];
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
