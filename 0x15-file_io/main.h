91aa3ec3d1e9e8188cc04facdf4f6977e86c05ae
baranosh <emmanuelmajor99@gmail.com>
2022-12-06 05:26:05 +0100

    user defined header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
