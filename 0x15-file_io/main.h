e7bbaa723c14ef60f7131217965ba093265b6c35
baranosh <emmanuelmajor99@gmail.com>
2022-12-06 05:08:42 +0100

    updated user defined header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,10 +2,13 @@
 #define _MAIN_H_
 
 #include <unistd.h>
+#include <stdlib.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *str);
 
 #endif
